3dddb5ffcd73e382f067c8c88bae76ecf78508fe
Steve <sbrennison@gmail.com>
2018-06-26 17:08:50 -0500

    commit 4 - added some style and h3s

--- js/app.js
+++ js/app.js
@@ -6,22 +6,31 @@ let time = 0;
 class Tamagotchi {
   constructor(name, age, hunger, boredom, sleepiness) {
     // this.name = prompt("Name this thing", this.name);
+    // form?
     this.age = 0;
     // go up a year every 15 seconds
     this.hunger = 0;
     this.boredom = 0;
     this.sleepiness = 0;
-    // one of three randomly goes up every 5 seconds
+    // How to display these?
+    // if >= 7, flash red
   }
   feed(){
+    console.log(this.hunger);
+    // this.hunger--;
   }
 
   play(){
+    console.log(this.boredom);
+    // this.boredom--;
   }
 
   sleep(){
+    console.log(this.sleepiness);
+    // this.sleepiness--;
   }
-
+   // one of three randomly goes up every 5 seconds
+   // Math.floor(Math.random()); -- see poke-a-square
   die(){
     // if hunger, sleepiness, or boredom hits 10
     console.log("I thought you loved me")
@@ -33,35 +42,34 @@ const birth = new Tamagotchi();
 
 // Have a setInterval for time, age, stats
 
+// Display age and stats
+
 // looks left or right depending on happiness
   // makes sound?
 
 $('.feed').on('click', (e) => {
   console.log("nom nom nom"); 
               //+ reduceHunger function by 1
-
 });
 
 $('.play').on('click', (e) => {
-  console.log("I love you, human"); 
+  console.log("I love you, hu-man"); 
               //+ reduceBoredom function by 1
-
 });
 
 $('.sleep').on('click', (e) => {
   console.log("ZZZZZZZZZ"); 
               //+ reduceSleepiness function by 1
-
 });
 
 
 // const setTimer = () => {
 
 //   const intervalId = setInterval(() => {
-//     time--;
-//     if(time === 0){
-//       clearInterval(intervalId); // stops timer
-//       round++
+//     time++;
+
+//     if(dead){
+//       clearInterval(intervalId); // stop timer
 //     }
 //     $('#timer').text('timer: ' + time + 's');
 
