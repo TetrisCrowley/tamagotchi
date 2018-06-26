f1a35eeda92adee98be61526b4aabb9c6e34db15
Steve <sbrennison@gmail.com>
2018-06-26 16:26:38 -0500

    commit 3 - got buttons working

--- js/app.js
+++ js/app.js
@@ -1,33 +1,25 @@
 
 let time = 0;
 
+// use one picture until age 5, use that one until age 10, maybe another at 15?
+
 class Tamagotchi {
   constructor(name, age, hunger, boredom, sleepiness) {
-    this.name = prompt("Name this thing", this.name);
+    // this.name = prompt("Name this thing", this.name);
     this.age = 0;
+    // go up a year every 15 seconds
     this.hunger = 0;
     this.boredom = 0;
     this.sleepiness = 0;
+    // one of three randomly goes up every 5 seconds
   }
   feed(){
-    $('.feed').on('click', (e) => {
-    console.log("nom nom nom" + reduceHunger);
-
-    });
   }
 
   play(){
-    $('.play').on('click', (e) => {
-    console.log("I love you, human" + reduceBoredom);
-
-    });
   }
 
   sleep(){
-    $('.sleep').on('click', (e) => {
-    console.log("ZZZZZZZZZ" + reduceSleepiness);
-
-    });
   }
 
   die(){
@@ -44,11 +36,23 @@ const birth = new Tamagotchi();
 // looks left or right depending on happiness
   // makes sound?
 
+$('.feed').on('click', (e) => {
+  console.log("nom nom nom"); 
+              //+ reduceHunger function by 1
 
+});
 
+$('.play').on('click', (e) => {
+  console.log("I love you, human"); 
+              //+ reduceBoredom function by 1
 
+});
 
+$('.sleep').on('click', (e) => {
+  console.log("ZZZZZZZZZ"); 
+              //+ reduceSleepiness function by 1
 
+});
 
 
 // const setTimer = () => {
